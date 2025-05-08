26e29cce855b4f649cc18db7f7ab469c34e795f8
bezdev <leo.bezroukov@gmail.com>
2025-05-07 23:39:06 -0700

    update event dispatcher

--- src/cpp/engine/EventDispatcher.h
+++ src/cpp/engine/EventDispatcher.h
@@ -16,11 +16,13 @@ enum class EventType : unsigned int
     NUM_EVENTS,
 };
 
-struct EntityPayload {
+struct EntityPayload
+{
     unsigned int entityId;
 };
 
-struct KeypressPayload {
+struct KeypressPayload
+{
     char key;
 };
 
@@ -35,26 +37,26 @@ struct Event
 class EventDispatcher
 {
 public:
-    using Callback = std::function<void(const Event&)>;
+    using Callback = std::function<void(const Event &)>;
 
     EventDispatcher()
     {
         m_Subscribers.fill({});
     }
 
-    void subscribe(EventType type, Callback callback)
+    void Subscribe(EventType type, Callback callback)
     {
         std::lock_guard<std::mutex> lock(m_SubscriberMutex);
         m_Subscribers[static_cast<size_t>(type)].emplace_back(std::move(callback));
     }
 
-    void publishAsync(const Event& event)
+    void Publish(const Event &event)
     {
         std::lock_guard<std::mutex> lock(m_EventQueueMutex);
         m_EventQueue.push(event);
     }
 
-    void dispatchEvents()
+    void Dispatch()
     {
         std::queue<Event> eventsToDispatch;
 
@@ -65,28 +67,23 @@ public:
 
         while (!eventsToDispatch.empty())
         {
-            const Event& event = eventsToDispatch.front();
-            notifySubscribers(event);
+            const Event &event = eventsToDispatch.front();
+
+            std::lock_guard<std::mutex> lock(m_SubscriberMutex);
+            const auto &callbacks = m_Subscribers[static_cast<size_t>(event.type)];
+            for (const auto &callback : callbacks)
+            {
+                callback(event);
+            }
+
             eventsToDispatch.pop();
         }
     }
 
 private:
-    void notifySubscribers(const Event& event)
-    {
-        std::lock_guard<std::mutex> lock(m_SubscriberMutex);
-
-        const auto& callbacks = m_Subscribers[static_cast<size_t>(event.type)];
-        for (const auto& callback : callbacks)
-        {
-            callback(event);
-        }
-    }
-
     std::array<std::vector<Callback>, static_cast<size_t>(EventType::NUM_EVENTS)> m_Subscribers;
     std::queue<Event> m_EventQueue;
 
     std::mutex m_SubscriberMutex;
     std::mutex m_EventQueueMutex;
-
 };
